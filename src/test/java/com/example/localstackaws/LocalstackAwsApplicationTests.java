da7abe95fd4f7436b7088cac8d36e352d3ef42ec
dkswnkk <jjoy9810@gmail.com>
2023-10-21 18:27:05 +0900

    Initial commit

---
+++ src/test/java/com/example/localstackaws/LocalstackAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.localstackaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalstackAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
